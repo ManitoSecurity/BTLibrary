c635400c44d8ec356faabfa4d1ed781355caf9fe
mastino <bgravelle@zagmail.gonzaga.edu>
2014-11-17 22:31:21 -0800

    version 0.2 tester program added

---
+++ BT_tester/BT_tester.ino
@@ -0,0 +1,76 @@
+/* File BT_tester
+ * Designed to try out the bluetooth library
+ * 17 Nov 2014
+ * Brian Gravelle
+*/
+
+#include <Arduino.h>
+#include "../BTLib/rn41_lib_sand.h" //this might need to change for windows
+
+#define masterMAC 123443211234
+#define slaveMAC 123443211234
+
+void masterTest(){
+  Serial.println("--- Start Master Test ---");
+  
+  char friendMac = slaveMAC;
+  char msg[256];
+  
+  bt.setAsMaster();
+  addFriend(friendMac);
+  delay(100);
+  
+  makeMasterConnection();
+  delay(100);
+  
+  Serial.println("--- Connection Section Complete ---");
+    
+  bt.sendMsg("Hello world :)");
+  delay(100);
+  
+  Serial.println("--- message sent ---");
+  
+  bt.recieveMsg(msg);
+  Serial.print("message recieved: ");
+  Serial.println(msg);
+  
+}
+
+void slaveTest(){
+  Serial.println("--- Start Slave Test ---");
+  
+  char friendMac = masterMAC;
+  char msg[256];
+    
+  bt.setAsSlave();
+  addFriend(friendMac);
+  delay(100);
+  
+  while(!bt.checkConnection()){}
+  
+  Serial.println("--- Connection Section Complete ---");
+  
+  bt.recieveMsg(msg);
+  Serial.print("message recieved: ");
+  Serial.println(msg);
+    
+  bt.sendMsg("yes master?");
+  delay(100);
+  
+  Serial.println("--- message sent ---");
+}
+
+void setup() {
+  bt = rn41();
+  bt.btInit(); 
+  Serial.begin(9600); 
+}
+
+void loop() 
+{  
+  masterTest();
+  delay(10000);
+  slaveTest();
+  delay(10000);
+}
+
